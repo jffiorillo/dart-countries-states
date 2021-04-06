91077930e77e416ac98c8745110298523776527e
Jose Fiorillo <joseff@fb.com>
2021-04-06 19:43:40 +0100

    Update example

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -1,10 +1,12 @@
 import 'package:dart_countries_states/country_provider.dart';
 import 'package:dart_countries_states/street_provider.dart';
 
-final countryProvider = CountryProvider();
-var countries = countryProvider.getCountries();
+main() {
+  final countryProvider = CountryProvider();
+  var countries = countryProvider.getCountries();
 
 // When looking for an specific country
-var country = countryProvider.getCountryByName("United States of America");
+  var country = countryProvider.getCountryByName("United States of America");
 
-var streets = StreetProvider().getStreetsByName("ca");
+  var streets = StreetProvider().getStreetsByName("ca");
+}
